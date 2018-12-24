fe173d652372666a10b41104eb68930ac5b000e5
Ayush Rangwala <aayushrangwala@gmail.com>
2018-12-25 00:40:01 +0530

    Directory update

--- ALGORITHMIC_TOOLBOX/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/FibonacciSumSquares.java
+++ ALGORITHMIC_TOOLBOX/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/FibonacciSumSquares.java
@@ -18,12 +18,43 @@ public class FibonacciSumSquares {
 
         return sum % 10;
     }
+
+
+    private static long getFibonacciSumSquaresFast (long n) {
+        if (n <= 1) {
+            return n;
+        }
+        long sum = 1, first = 0, sec = 1;
+        for (int i = 2; i <= n; i++) {
+            long tmp = (first%10 + sec%10) % 10;
+            sum += (tmp*tmp)%10;
+            first = sec;
+            sec = tmp;
+        }
+        return sum%10;
+    }
+
+    private static void stressTest () {
+        while (true) {
+            Random r = new Random();
+            long n = r.nextLong();
+            long res = getFibonacciSumSquaresNaive(n);
+            long res1 = getFibonacciSumSquaresFast(n);
+            if (res == res1) {
+                System.out.println("OK");
+            } else {
+                System.out.println("Wrong Ans: " + res + " " + res1 + " " + n);
+            }
+        }
+    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumSquaresNaive(n);
-        System.out.println(s);
+        long x = 60;
+        n %= x;
+        System.out.println(getFibonacciSumSquaresFast(n));
+        //stressTest();
     }
 }
 
