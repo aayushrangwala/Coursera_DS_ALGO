64334d4882acf136f6965adaa24e186d80532d82
ayushr <arangwala@hytrust.com>
2018-12-31 19:27:28 +0530

    week4-qns-1-1st-draft and week3-qn-4-NP

--- ALGORITHMIC_TOOLBOX/week3_greedy_algorithms/4_collecting_signatures/CoveringSegments.java
+++ ALGORITHMIC_TOOLBOX/week3_greedy_algorithms/4_collecting_signatures/CoveringSegments.java
@@ -2,14 +2,46 @@ import java.util.*;
 
 public class CoveringSegments {
 
-    private static int[] optimalPoints(Segment[] segments) {
+    private static List<Integer> optimalPoints(Segment[] segments) {
         //write your code here
-        int[] points = new int[2 * segments.length];
+        Arrays.sort(segments, new Comparator<Segment> () {
+            @Override
+            public int compare (Segment a, Segment b) {
+                int x = a.end;
+                int y = b.end;
+                if (x == y) {
+                    return 0;
+                } else if (x > y) {
+                    return 1;
+                }
+                return -1;
+            } 
+        });
+        
+
+        List<Integer> list = new ArrayList<Integer>();
+        int i = 0, j = 0;
+        while (i < segments.length-1 && j < segments.length) {
+            if (segments[i].start > segments[j].end) {
+                list.add(segments[j].end);
+                j++;
+                continue;
+            } else if (segments[i].start == segments[j].end) {
+                j++;
+                continue;
+            }
+            i++;
+        }
+        if (i == segments.length-1 && j <= segments.length-1 && segments[i].start <= segments[j].end) {
+            list.add(segments[j].end);
+        }
+
+        /*int[] points = new int[2 * segments.length];
         for (int i = 0; i < segments.length; i++) {
             points[2 * i] = segments[i].start;
             points[2 * i + 1] = segments[i].end;
-        }
-        return points;
+        }*/
+        return list;
     }
 
     private static class Segment {
@@ -30,8 +62,9 @@ public class CoveringSegments {
             end = scanner.nextInt();
             segments[i] = new Segment(start, end);
         }
-        int[] points = optimalPoints(segments);
-        System.out.println(points.length);
+        //int[] points = optimalPoints(segments);
+        List<Integer> points = optimalPoints(segments);
+        System.out.println(points.size());
         for (int point : points) {
             System.out.print(point + " ");
         }
--- ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/1_binary_search/BinarySearch.java
+++ ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/1_binary_search/BinarySearch.java
@@ -1,12 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class BinarySearch {
 
-    static int binarySearch(int[] a, int x) {
+class BinarySearch {
+
+    static int binarySearch(int[] a, int key) {
+        Arrays.sort(a);
+        if (key < a[0] || key > a[a.length-1]) {
+            return -1;
+        }
         int left = 0, right = a.length;
+        //quickSort(a, left, right);
         //write your code here
+        return binarySearchUtil(a, left, right, key);
+    }
 
+    /*static int binarySearchUtil (int[] a, int l, int r, int key) {
+        if (l < r) {
+            int mid = l + (r-l)/2;
+            if  (a[mid] == key) {
+                return mid;
+            }
+            if (a[mid] > key) {
+                return binarySearchUtil(a, l, mid, key);
+            }
+            return binarySearchUtil(a, mid+1, r, key);
+        }
+        return -1;
+    }*/
+
+    static int binarySearchUtil (int[] a, int l, int r, int key) {
+        while (l < r-1) {
+            int mid = l + (r-l)/2;
+            if  (a[mid] <= key) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+        if (a[l] == key) {
+            return l;
+        }
         return -1;
     }
 
@@ -29,10 +63,14 @@ public class BinarySearch {
         for (int i = 0; i < m; i++) {
           b[i] = scanner.nextInt();
         }
+        
         for (int i = 0; i < m; i++) {
             //replace with the call to binarySearch when implemented
-            System.out.print(linearSearch(a, b[i]) + " ");
+            System.out.print(binarySearch(a, b[i]) + " ");
+            
         }
+        //quickSort(a, 0, a.length-1);
+        //System.out.println(Arrays.toString(a));
     }
     static class FastScanner {
         BufferedReader br;
@@ -61,4 +99,51 @@ public class BinarySearch {
             return Integer.parseInt(next());
         }
     }
+    
+    /*public static void quickSort (int[] arr, int l, int r) {
+        while (l <= r) {
+            Pair p = partition(arr, l, r);
+            int p1 = p.p1;
+            int p2 = p.p2;
+            if (p1-l-1 < r-p2-1) {
+                quickSort(arr, l, p1-1);
+                l = p2+1;
+            } else {
+                quickSort(arr, p2+1, r);
+                r = p1-1;
+            }
+        }
+    }
+    
+    public static Pair partition (int[] arr, int l, int r) {
+        if (r-l <= 1) {
+            if (arr[r] < arr[l]) {
+                swap(arr, l, r);
+            }
+            return new Pair(l, r);
+        }
+        int left = l, mid = l, right = r;
+        int pivot = arr[r];
+        while (mid <= right) {
+            if (arr[mid] < pivot) {
+                swap(arr, mid, left);
+                left++;
+                mid++;
+            } else if (arr[mid] == pivot) {
+                mid++;
+            } else {
+                swap(arr, mid, right);
+                right--;
+            }
+        }
+        return new Pair(left-1, mid);
+    }
+    
+    public static void swap (int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }*/
+    
+    
 }
