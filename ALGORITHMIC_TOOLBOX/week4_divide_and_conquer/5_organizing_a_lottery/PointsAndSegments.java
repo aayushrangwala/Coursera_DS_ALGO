9727424ed773f210514a3bb606d376013f0ea36b
Ayush Rangwala <aayushrangwala@gmail.com>
2019-01-02 21:46:33 +0530

    week 4 Points and Segments qn 5 Done

--- ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/5_organizing_a_lottery/PointsAndSegments.java
+++ ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/5_organizing_a_lottery/PointsAndSegments.java
@@ -35,13 +35,26 @@ public class PointsAndSegments {
                 int v2 = b.value;
 
                 if (v1 == v2) {
-                    return 0;
+                    if ("p".equals(a.type) && "l".equals(b.type)) {
+                        return 1;
+                    } else if ("r".equals(a.type) && "p".equals(b.type)) {
+                        return 1;
+                    } else if ("r".equals(a.type) && "l".equals(b.type)) {
+                        return 1;
+                    } else if ((a.type).equals(b.type)) {
+                        return 0;
+                    }
+                    return -1;
                 }
                 return (v1 > v2) ? 1: -1;
             }
         });
-        Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-        
+    
+        /*for (Pair pp : list) {
+            if ("p".equals(pp.type)) {
+                System.out.println(pp.value + " " + pp.index);
+            }
+        }*/
         
         int leftCount = 0;
         for (Pair p : list) {
@@ -52,8 +65,12 @@ public class PointsAndSegments {
                 leftCount++;
             else if (t.equals("r"))
                 leftCount--;
-            else
-                cnt[ind] = leftCount;
+            else {
+                if (leftCount < 0) {
+                    leftCount = 0;
+                }
+                cnt[ind] += leftCount;
+            }
         }
         return cnt;
     }
@@ -90,6 +107,47 @@ public class PointsAndSegments {
         for (int x : cnt) {
             System.out.print(x + " ");
         }
+        //stressTest(50000, 50000, 10000000);
     }
+
+    /*public static void stressTest (int N, int M, int a) {
+        Random r = new Random();
+        //int l = 1;
+        while (true) {
+            int n = r.nextInt(N);
+            int m = r.nextInt(M);
+            int[] s = new int[n];
+            int[] e = new int[n];
+            int[] p = new int[m];
+            for (int i = 0; i < n; i++) {
+                s[i] = r.nextInt(a);
+                e[i] = r.nextInt(a) + s[i];
+            }
+            for (int i = 0; i < m; i++) {
+                p[i] = r.nextInt(a);
+            }
+            int[] cnt1 = fastCountSegments(s, e, p);
+            int[] cnt2 = naiveCountSegments(s, e, p);
+            boolean flag = true;
+            for (int i = 0; i < m; i++) {
+                if (cnt1[i] != cnt2[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                System.out.println("OK");
+            } else {
+                System.out.println(Arrays.toString(s));
+                System.out.println(Arrays.toString(e));
+                System.out.println(Arrays.toString(p));
+                System.out.println(Arrays.toString(cnt1));
+                System.out.println(Arrays.toString(cnt2));
+                break;
+            }
+            //l--;
+        }
+    }*/
 }
 
