38aa5ac39f1fa9795ef909ec387f0e7f0b7f5f38
ayushr <arangwala@hytrust.com>
2019-01-02 17:24:54 +0530

    week4 qn 5- not complete

--- ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/5_organizing_a_lottery/PointsAndSegments.java
+++ ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/5_organizing_a_lottery/PointsAndSegments.java
@@ -1,10 +1,60 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class PointsAndSegments {
+
+    static class Pair {
+        int value;
+        String type;
+        int index;
+
+        Pair (int v, String t, int i) {
+            value = v;
+            type = t;
+            index = i;
+        }
+    }
 
     private static int[] fastCountSegments(int[] starts, int[] ends, int[] points) {
         int[] cnt = new int[points.length];
         //write your code here
+        List<Pair> list = new ArrayList<Pair>();
+        for (int i = 0; i < starts.length; i++) {
+            list.add(new Pair(starts[i], "l", i));
+        }
+        for (int i = 0; i < ends.length; i++) {
+            list.add(new Pair(ends[i], "r", i));
+        }
+        for (int i = 0; i < points.length; i++) {
+            list.add(new Pair(points[i], "p", i));
+        }
+
+        Collections.sort(list, new Comparator<Pair> () {
+            @Override
+            public int compare (Pair a, Pair b) {
+                int v1 = a.value;
+                int v2 = b.value;
+
+                if (v1 == v2) {
+                    return 0;
+                }
+                return (v1 > v2) ? 1: -1;
+            }
+        });
+        Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
+        
+        
+        int leftCount = 0;
+        for (Pair p : list) {
+            int v = p.value;
+            int ind = p.index;
+            String t = p.type;
+            if (t.equals("l"))
+                leftCount++;
+            else if (t.equals("r"))
+                leftCount--;
+            else
+                cnt[ind] = leftCount;
+        }
         return cnt;
     }
 
@@ -36,7 +86,7 @@ public class PointsAndSegments {
             points[i] = scanner.nextInt();
         }
         //use fastCountSegments
-        int[] cnt = naiveCountSegments(starts, ends, points);
+        int[] cnt = fastCountSegments(starts, ends, points);
         for (int x : cnt) {
             System.out.print(x + " ");
         }
