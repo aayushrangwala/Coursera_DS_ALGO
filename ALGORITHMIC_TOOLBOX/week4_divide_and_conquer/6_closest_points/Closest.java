ec07e1d4fe4772cf82a9d37fa14f1dcfb10b1491
Ayush Rangwala <aayushrangwala@gmail.com>
2019-01-04 21:30:11 +0530

    corrections in syntax but still not working

--- ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/6_closest_points/Closest.java
+++ ALGORITHMIC_TOOLBOX/week4_divide_and_conquer/6_closest_points/Closest.java
@@ -85,9 +85,9 @@ public class Closest {
     public static double stripClosest (List<Point> list, double d) {
         double min = d;
         for (int i = 0; i < list.size(); i++) {
-            for (int j = i+1; j < list.size() && (list[i].y - list[j].y) < min; j++) {//It is proofed that for every i,
+            for (int j = i+1; j < list.size() && (list.get(i).y - list.get(j).y) < min; j++) {//It is proofed that for every i,
                 //there are at most '7' j such that (point[i]-point[j]) > 0, means fter 7th J dist will increase
-                double tmp = distance(list[i], list[j]);
+                double tmp = distance(list.get(i), list.get(j));
                 min = (min > tmp) ? tmp : min;
             }
         }
